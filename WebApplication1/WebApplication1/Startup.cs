ef93479c4356769496c95d095bb0ce9e3ea03605
欧阳 <loongouyang@gmail.com>
2020-02-25 12:12:49 +0800

    新增项目手机号码验证

--- WebApplication1/WebApplication1/Startup.cs
+++ WebApplication1/WebApplication1/Startup.cs
@@ -5,24 +5,16 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace MyCoreProject
+namespace WebApplication1
 {
     public class Startup
     {
-        private readonly IConfiguration _configuration;
-        public Startup(IConfiguration configuration)
-        {
-            _configuration = configuration;
-        }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddScoped<IWebHostBuilder, WebHostBuilder>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,16 +25,9 @@ namespace MyCoreProject
                 app.UseDeveloperExceptionPage();
             }
 
-            var processName = System.Diagnostics.Process.GetCurrentProcess().ProcessName;
-            var machineName = System.Diagnostics.Process.GetCurrentProcess().MachineName;
-
             app.Run(async (context) =>
             {
-                var key = _configuration["Key"];
-                await context.Response.WriteAsync(key);
                 await context.Response.WriteAsync("Hello World!");
-                await context.Response.WriteAsync(processName);
-                await context.Response.WriteAsync(machineName);
             });
         }
     }
---
+++ 简单手机号码验证/Common/PhoneValidator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Text.RegularExpressions;
+
+namespace 简单手机号码验证.Common
+{
+    public class PhoneValidator
+    {
+        private static readonly Regex checktor = new Regex(@"^1\d{10}$");
+        public IDictionary segment = null;
+
+        public PhoneValidator(IDictionary segment)
+        {
+            this.segment = segment;
+        }
+
+        public bool IsPhone(ref string tel)
+        {
+            if (string.IsNullOrEmpty(tel))
+            {
+                return false;
+            }
+
+            tel = tel.Replace("+86-", "").Replace("+86", "").Replace("86-", "").Replace("-", "");
+            if (!checktor.IsMatch(tel))
+            {
+                return false;
+            }
+            string s = tel.Substring(0, 3);
+            if (segment.Count > 0 && !segment.Contains(s))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
