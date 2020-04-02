665004db6981802c279f7b0c0c28bdabd98afc5f
欧阳 <loongouyang@gmail.com>
2020-04-02 19:48:25 +0800

    分支合并测试

--- src/SignalR/SignalRChat/Hubs/ChatHub.cs
+++ src/SignalR/SignalRChat/Hubs/ChatHub.cs
@@ -10,6 +10,7 @@ namespace SignalRChat.Hubs
     {
         public async Task SendMessage(string user, string message)
         {
+            //消息接收然后发送
             await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
     }
