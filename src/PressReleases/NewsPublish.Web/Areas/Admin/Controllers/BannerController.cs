50cff01de374094f055165eefcfcfb9146573fa2
欧阳 <225948660@qq.com>
2020-04-10 01:23:32 +0800

    完成了banner的页面和功能

---
+++ src/PressReleases/NewsPublish.Web/Areas/Admin/Controllers/BannerController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NewsPublish.Models.Request;
+using NewsPublish.Models.Response;
+using NewsPublish.Service;
+using Microsoft.Extensions.Hosting;
+using System.IO;
+
+namespace NewsPublish.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BannerController : Controller
+    {
+        BannerService _bannerService;
+        IHostEnvironment _hostEnvironment;
+
+        public BannerController(BannerService bannerService, IHostEnvironment hostEnvironment)
+        {
+            _bannerService = bannerService;
+            _hostEnvironment = hostEnvironment;
+        }
+        // GET: Banner
+        public ActionResult Index()
+        {
+            var bannerList = _bannerService.GetBannerList();
+            return View(bannerList);
+        }
+
+        // GET: Banner/Details/5
+        public ActionResult BannerAdd(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> AddBanner(AddBannerDto dto, IFormFile uploadFile)
+        {
+            if (uploadFile == null)
+                return Json(new ResponseModel { Code = 0, Result = "请上传图片！" });
+
+            var webRootPath = _hostEnvironment.ContentRootPath;
+            var relativeDirPath = "BannerPic";
+            var absolutePath = Path.Combine(webRootPath, relativeDirPath);
+
+            var fileTypes = new string[] { ".gif", ".jpg", ".jpeg", ".png", ".bmp" };
+            var extension = Path.GetExtension(uploadFile.FileName);
+            if (!fileTypes.Contains(extension.ToLower()))
+                return Json(new ResponseModel { Code = 0, Result = "图片格式有误！" });
+
+            if (!Directory.Exists(absolutePath)) Directory.CreateDirectory(absolutePath);
+
+            var fileName = DateTime.Now.ToString("yyyyMMddHHmmss") + extension;
+            var filePath = Path.Combine(absolutePath, fileName);
+            await using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await uploadFile.CopyToAsync(stream);
+            }
+            dto.Image = Path.Combine("/BannerPic/", fileName);
+            return Json(_bannerService.AddBanner(dto));
+        }
+
+        [HttpDelete]
+        public JsonResult DelBanner(int id)
+        {
+            if (id <= 0)
+                return Json(new ResponseModel { Code = 0, Result = "参数错误！" });
+            return Json(_bannerService.DeleteBanne(id));
+        }
+    }
+}
--- src/PressReleases/NewsPublish.Web/Startup.cs
+++ src/PressReleases/NewsPublish.Web/Startup.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -7,7 +8,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
+using NewsPublish.Models;
+using NewsPublish.Service;
 
 namespace NewsPublish.Web
 {
@@ -24,6 +28,10 @@ namespace NewsPublish.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddTransient<MyDbContext>();
+            services.AddTransient<BannerService>();
+            services.AddTransient<NewsService>();
+            services.AddTransient<NewsCommentService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,13 +49,21 @@ namespace NewsPublish.Web
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(
+                    Path.Combine(Directory.GetCurrentDirectory(), "BannerPic")),
+                RequestPath = "/BannerPic"
+            });
             app.UseRouting();
 
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllerRoute(
+                    name: "areaRoute",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
