fd1a0652c10a6b3e86a250e3be4defa0e070417b
欧阳 <loongouyang@gmail.com>
2020-04-12 21:30:39 +0800

    开始前台页面制作

--- src/PressReleases/NewsPublish.Web/Controllers/HomeController.cs
+++ src/PressReleases/NewsPublish.Web/Controllers/HomeController.cs
@@ -6,32 +6,33 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using NewsPublish.Web.Models;
+using NewsPublish.Service;
 
 namespace NewsPublish.Web.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly NewsService _newsSerrvice;
+        private readonly BannerService _bannerService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, NewsService newsSerrvice, BannerService bannerService)
         {
             _logger = logger;
+            _newsSerrvice = newsSerrvice;
+            _bannerService = bannerService;
         }
 
         public IActionResult Index()
         {
-            return View();
+            ViewData["Title"] = "首页";
+            var newsClassifies = _newsSerrvice.GetNewsClassifyList();
+            return View(newsClassifies);
         }
-
-        public IActionResult Privacy()
+        [HttpGet]
+        public JsonResult GetBanner()
         {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return Json(_bannerService.GetBannerList());
         }
     }
 }
