7c9f1d52ccd01fc496d7f95badf2737273ec7752
欧阳奕龙 <aaronouy@qq.com>
2021-03-26 15:36:08 +0800

    增加产品页面代码

---
+++ src/ProductWeb/ProductWeb/Models/ProductModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductWeb.Models
+{
+    public class ProductModel
+    {
+        public int Id { get; set; }
+        public string ProductNo { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
