2eb75e22d877257412d8018584329db4332840e9
欧阳 <225948660@qq.com>
2021-07-15 23:48:00 +0800

    增加一个winfrom项目

---
+++ src/SelfHelp/SelfHelp/SelfHelp/CEFKeyBoardHandler.cs
@@ -0,0 +1,68 @@
+﻿using CefSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SelfHelp
+{
+    public class CEFKeyBoardHandler : IKeyboardHandler
+    {
+        public bool OnPreKeyEvent(IWebBrowser chromiumWebBrowser, IBrowser browser, KeyType type, int windowsKeyCode,
+            int nativeKeyCode, CefEventFlags modifiers, bool isSystemKey, ref bool isKeyboardShortcut)
+        {
+            const int VK_F1 = 0x70;
+            const int VK_F5 = 0x74;
+            const int VK_F11 = 0x7A;
+            const int VK_F12 = 0x7B;
+            var control = chromiumWebBrowser as Control;
+            switch (windowsKeyCode)
+            {
+                case VK_F1:
+                    MainForm._mainForm.BeginInvoke(new Action(() =>
+                    {
+                        MainForm._mainForm.FormBorderStyle = FormBorderStyle.FixedSingle;
+                        MainForm._mainForm.WindowState = FormWindowState.Normal;
+                    }));
+                    break;
+                case VK_F5:
+                    if (modifiers == CefEventFlags.ControlDown)
+                    {
+                        browser.Reload(true); //强制忽略缓存
+                    }
+                    else
+                    {
+                        browser.Reload();
+                    }
+                    break;
+                case VK_F11:
+                    MainForm._mainForm.BeginInvoke(new Action(() =>
+                    {
+                        MainForm._mainForm.FormBorderStyle = FormBorderStyle.None;
+                        MainForm._mainForm.WindowState = FormWindowState.Maximized;
+                    }));
+                    break;
+                case VK_F12:
+                    browser.ShowDevTools();
+                    break;
+            }
+
+            return false;
+        }
+
+        public bool OnKeyEvent(IWebBrowser chromiumWebBrowser, IBrowser browser, KeyType type, int windowsKeyCode, int nativeKeyCode,
+            CefEventFlags modifiers, bool isSystemKey)
+        {
+            return false;
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+
+        }
+    }
+
+}
---
+++ src/SelfHelp/SelfHelp/SelfHelp/MainForm.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.Handler;
+using CefSharp.WinForms;
+
+namespace SelfHelp
+{
+    public partial class MainForm : Form
+    {
+        public static MainForm _mainForm;
+        ChromiumWebBrowser browser;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            _mainForm = this;
+            //去除顶栏
+            this.FormBorderStyle = FormBorderStyle.None;
+            //设置全屏展示
+            this.WindowState = FormWindowState.Maximized;
+
+            var settings = new CefSettings
+            {
+                Locale = "zh-CN"
+            };
+
+            if (!Cef.IsInitialized)
+                Cef.Initialize(settings);
+
+            var webUrl = ConfigurationManager.AppSettings["webUrl"].Trim();
+            browser = new ChromiumWebBrowser(webUrl)
+            {
+                Dock = DockStyle.Fill,
+                KeyboardHandler = new CEFKeyBoardHandler()
+            };
+            this.Controls.Add(browser);
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            // 结束时要销毁
+            browser.CloseDevTools();
+            browser.GetBrowser().CloseBrowser(true);
+            browser.Dispose();
+            browser = null;
+            Cef.Shutdown();
+        }
+
+    }
+}
