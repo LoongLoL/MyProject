df3a946aaa2e2da4e379621003f452103c4fd45d
欧阳 <225948660@qq.com>
2021-07-16 20:03:01 +0800

    修改文件

--- src/SelfHelp/SelfHelp/SelfHelp/CEFKeyBoardHandler.cs
+++ src/SelfHelp/SelfHelp/SelfHelp/CEFKeyBoardHandler.cs
@@ -9,26 +9,19 @@ using System.Windows.Forms;
 
 namespace SelfHelp
 {
-    public class CEFKeyBoardHandler : IKeyboardHandler
+    public class CefKeyBoardHandler : IKeyboardHandler
     {
+        public bool IsMaxWindow = true;
         public bool OnPreKeyEvent(IWebBrowser chromiumWebBrowser, IBrowser browser, KeyType type, int windowsKeyCode,
             int nativeKeyCode, CefEventFlags modifiers, bool isSystemKey, ref bool isKeyboardShortcut)
         {
-            const int VK_F1 = 0x70;
-            const int VK_F5 = 0x74;
-            const int VK_F11 = 0x7A;
-            const int VK_F12 = 0x7B;
-            var control = chromiumWebBrowser as Control;
+            const int vkF5 = 0x74;
+            const int vkF11 = 0x7A;
+            const int vkF12 = 0x7B;
+
             switch (windowsKeyCode)
             {
-                case VK_F1:
-                    MainForm._mainForm.BeginInvoke(new Action(() =>
-                    {
-                        MainForm._mainForm.FormBorderStyle = FormBorderStyle.FixedSingle;
-                        MainForm._mainForm.WindowState = FormWindowState.Normal;
-                    }));
-                    break;
-                case VK_F5:
+                case vkF5:
                     if (modifiers == CefEventFlags.ControlDown)
                     {
                         browser.Reload(true); //强制忽略缓存
@@ -37,17 +30,27 @@ namespace SelfHelp
                     {
                         browser.Reload();
                     }
-                    break;
-                case VK_F11:
+                    return true;
+                case vkF11:
                     MainForm._mainForm.BeginInvoke(new Action(() =>
                     {
-                        MainForm._mainForm.FormBorderStyle = FormBorderStyle.None;
-                        MainForm._mainForm.WindowState = FormWindowState.Maximized;
+                        if (IsMaxWindow)
+                        {
+                            MainForm._mainForm.FormBorderStyle = FormBorderStyle.FixedSingle;
+                            MainForm._mainForm.WindowState = FormWindowState.Normal;
+                            IsMaxWindow = !IsMaxWindow;
+                        }
+                        else
+                        {
+                            MainForm._mainForm.FormBorderStyle = FormBorderStyle.None;
+                            MainForm._mainForm.WindowState = FormWindowState.Maximized;
+                            IsMaxWindow = !IsMaxWindow;
+                        }
                     }));
-                    break;
-                case VK_F12:
+                    return true;
+                case vkF12:
                     browser.ShowDevTools();
-                    break;
+                    return true;
             }
 
             return false;
